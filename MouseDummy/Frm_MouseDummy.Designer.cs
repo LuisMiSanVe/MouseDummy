d95a8303829c851ff8c9655b7ef472fe6e648ca9
Luis Miguel Sánchez <luis.miguel.sanve@gmail.com>
2025-05-16 22:20:27 +0200

    Add files via upload

--- MouseDummy/Frm_MouseDummy.Designer.cs
+++ MouseDummy/Frm_MouseDummy.Designer.cs
@@ -29,42 +29,78 @@
         private void InitializeComponent()
         {
             grpbx_points = new GroupBox();
-            grpbx_pointSelector = new GroupBox();
-            grpbx_action = new GroupBox();
+            btn_playSecuence = new Button();
+            cmbbx_savedSecuences = new ComboBox();
             lstbx_points = new ListBox();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            lbl_x = new Label();
-            lbl_y = new Label();
+            grpbx_pointSelector = new GroupBox();
+            btn_trackMousePos = new Button();
             btn_add = new Button();
-            btn_trackMouseCoor = new Button();
-            tabControl1 = new TabControl();
-            tabPage1 = new TabPage();
-            tabPage2 = new TabPage();
+            lbl_y = new Label();
+            lbl_x = new Label();
+            txtbx_x = new TextBox();
+            txtbx_y = new TextBox();
+            grpbx_action = new GroupBox();
+            btn_moveInstant = new Button();
+            btn_scrollDown = new Button();
+            btn_scrollUp = new Button();
+            btn_clickRight = new Button();
+            btn_clickLeft = new Button();
+            btn_moveGradual = new Button();
+            lbl_scroll = new Label();
+            lbl_click = new Label();
+            lbl_move = new Label();
             grpbx_points.SuspendLayout();
             grpbx_pointSelector.SuspendLayout();
             grpbx_action.SuspendLayout();
-            tabControl1.SuspendLayout();
             SuspendLayout();
             // 
             // grpbx_points
             // 
+            grpbx_points.Controls.Add(btn_playSecuence);
+            grpbx_points.Controls.Add(cmbbx_savedSecuences);
             grpbx_points.Controls.Add(lstbx_points);
             grpbx_points.Location = new Point(12, 12);
             grpbx_points.Name = "grpbx_points";
-            grpbx_points.Size = new Size(277, 426);
+            grpbx_points.Size = new Size(277, 289);
             grpbx_points.TabIndex = 0;
             grpbx_points.TabStop = false;
             grpbx_points.Text = "Points";
             // 
+            // btn_playSecuence
+            // 
+            btn_playSecuence.Location = new Point(233, 19);
+            btn_playSecuence.Name = "btn_playSecuence";
+            btn_playSecuence.Size = new Size(25, 25);
+            btn_playSecuence.TabIndex = 2;
+            btn_playSecuence.UseVisualStyleBackColor = true;
+            // 
+            // cmbbx_savedSecuences
+            // 
+            cmbbx_savedSecuences.FormattingEnabled = true;
+            cmbbx_savedSecuences.Items.AddRange(new object[] { "+ Save the current secuence..." });
+            cmbbx_savedSecuences.Location = new Point(16, 19);
+            cmbbx_savedSecuences.Name = "cmbbx_savedSecuences";
+            cmbbx_savedSecuences.Size = new Size(211, 23);
+            cmbbx_savedSecuences.TabIndex = 1;
+            cmbbx_savedSecuences.SelectedIndexChanged += cmbbx_savedSecuences_SelectedIndexChanged;
+            // 
+            // lstbx_points
+            // 
+            lstbx_points.FormattingEnabled = true;
+            lstbx_points.ItemHeight = 15;
+            lstbx_points.Location = new Point(16, 48);
+            lstbx_points.Name = "lstbx_points";
+            lstbx_points.Size = new Size(242, 229);
+            lstbx_points.TabIndex = 0;
+            // 
             // grpbx_pointSelector
             // 
-            grpbx_pointSelector.Controls.Add(btn_trackMouseCoor);
+            grpbx_pointSelector.Controls.Add(btn_trackMousePos);
             grpbx_pointSelector.Controls.Add(btn_add);
             grpbx_pointSelector.Controls.Add(lbl_y);
             grpbx_pointSelector.Controls.Add(lbl_x);
-            grpbx_pointSelector.Controls.Add(textBox2);
-            grpbx_pointSelector.Controls.Add(textBox1);
+            grpbx_pointSelector.Controls.Add(txtbx_x);
+            grpbx_pointSelector.Controls.Add(txtbx_y);
             grpbx_pointSelector.Location = new Point(295, 12);
             grpbx_pointSelector.Name = "grpbx_pointSelector";
             grpbx_pointSelector.Size = new Size(291, 100);
@@ -72,47 +108,25 @@
             grpbx_pointSelector.TabStop = false;
             grpbx_pointSelector.Text = "Point Select";
             // 
-            // grpbx_action
+            // btn_trackMousePos
             // 
-            grpbx_action.Controls.Add(tabControl1);
-            grpbx_action.Location = new Point(295, 118);
-            grpbx_action.Name = "grpbx_action";
-            grpbx_action.Size = new Size(291, 320);
-            grpbx_action.TabIndex = 2;
-            grpbx_action.TabStop = false;
-            grpbx_action.Text = "Actions";
+            btn_trackMousePos.Location = new Point(14, 58);
+            btn_trackMousePos.Name = "btn_trackMousePos";
+            btn_trackMousePos.Size = new Size(130, 23);
+            btn_trackMousePos.TabIndex = 5;
+            btn_trackMousePos.Text = "Track Mouse Position";
+            btn_trackMousePos.UseVisualStyleBackColor = true;
+            btn_trackMousePos.Click += btn_trackMousePos_Click;
             // 
-            // lstbx_points
+            // btn_add
             // 
-            lstbx_points.FormattingEnabled = true;
-            lstbx_points.ItemHeight = 15;
-            lstbx_points.Location = new Point(6, 18);
-            lstbx_points.Name = "lstbx_points";
-            lstbx_points.Size = new Size(265, 394);
-            lstbx_points.TabIndex = 0;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(173, 20);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(107, 23);
-            textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(37, 20);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(107, 23);
-            textBox2.TabIndex = 1;
-            // 
-            // lbl_x
-            // 
-            lbl_x.AutoSize = true;
-            lbl_x.Location = new Point(14, 23);
-            lbl_x.Name = "lbl_x";
-            lbl_x.Size = new Size(17, 15);
-            lbl_x.TabIndex = 2;
-            lbl_x.Text = "X:";
+            btn_add.Location = new Point(150, 58);
+            btn_add.Name = "btn_add";
+            btn_add.Size = new Size(130, 23);
+            btn_add.TabIndex = 4;
+            btn_add.Text = "Add (N)";
+            btn_add.UseVisualStyleBackColor = true;
+            btn_add.Click += btn_add_Click;
             // 
             // lbl_y
             // 
@@ -123,69 +137,150 @@
             lbl_y.TabIndex = 3;
             lbl_y.Text = "Y:";
             // 
-            // btn_add
+            // lbl_x
             // 
-            btn_add.Location = new Point(150, 58);
-            btn_add.Name = "btn_add";
-            btn_add.Size = new Size(130, 23);
-            btn_add.TabIndex = 4;
-            btn_add.Text = "Add (N)";
-            btn_add.UseVisualStyleBackColor = true;
+            lbl_x.AutoSize = true;
+            lbl_x.Location = new Point(14, 23);
+            lbl_x.Name = "lbl_x";
+            lbl_x.Size = new Size(17, 15);
+            lbl_x.TabIndex = 2;
+            lbl_x.Text = "X:";
             // 
-            // btn_trackMouseCoor
+            // txtbx_x
             // 
-            btn_trackMouseCoor.Location = new Point(14, 58);
-            btn_trackMouseCoor.Name = "btn_trackMouseCoor";
-            btn_trackMouseCoor.Size = new Size(130, 23);
-            btn_trackMouseCoor.TabIndex = 5;
-            btn_trackMouseCoor.Text = "Track Mouse Position";
-            btn_trackMouseCoor.UseVisualStyleBackColor = true;
+            txtbx_x.Location = new Point(37, 20);
+            txtbx_x.Name = "txtbx_x";
+            txtbx_x.Size = new Size(107, 23);
+            txtbx_x.TabIndex = 1;
             // 
-            // tabControl1
+            // txtbx_y
             // 
-            tabControl1.Controls.Add(tabPage1);
-            tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(14, 22);
-            tabControl1.Name = "tabControl1";
-            tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(266, 288);
-            tabControl1.TabIndex = 0;
+            txtbx_y.Location = new Point(173, 20);
+            txtbx_y.Name = "txtbx_y";
+            txtbx_y.Size = new Size(107, 23);
+            txtbx_y.TabIndex = 0;
             // 
-            // tabPage1
+            // grpbx_action
             // 
-            tabPage1.Location = new Point(4, 24);
-            tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(258, 260);
-            tabPage1.TabIndex = 0;
-            tabPage1.Text = "tabPage1";
-            tabPage1.UseVisualStyleBackColor = true;
+            grpbx_action.Controls.Add(btn_moveInstant);
+            grpbx_action.Controls.Add(btn_scrollDown);
+            grpbx_action.Controls.Add(btn_scrollUp);
+            grpbx_action.Controls.Add(btn_clickRight);
+            grpbx_action.Controls.Add(btn_clickLeft);
+            grpbx_action.Controls.Add(btn_moveGradual);
+            grpbx_action.Controls.Add(lbl_scroll);
+            grpbx_action.Controls.Add(lbl_click);
+            grpbx_action.Controls.Add(lbl_move);
+            grpbx_action.Location = new Point(295, 118);
+            grpbx_action.Name = "grpbx_action";
+            grpbx_action.Size = new Size(291, 183);
+            grpbx_action.TabIndex = 2;
+            grpbx_action.TabStop = false;
+            grpbx_action.Text = "Actions";
             // 
-            // tabPage2
+            // btn_moveInstant
             // 
-            tabPage2.Location = new Point(4, 24);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(192, 72);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "tabPage2";
-            tabPage2.UseVisualStyleBackColor = true;
+            btn_moveInstant.Location = new Point(205, 24);
+            btn_moveInstant.Name = "btn_moveInstant";
+            btn_moveInstant.Size = new Size(75, 23);
+            btn_moveInstant.TabIndex = 8;
+            btn_moveInstant.Text = "Instant";
+            btn_moveInstant.UseVisualStyleBackColor = true;
+            btn_moveInstant.Click += btn_action_Click;
+            // 
+            // btn_scrollDown
+            // 
+            btn_scrollDown.Location = new Point(205, 141);
+            btn_scrollDown.Name = "btn_scrollDown";
+            btn_scrollDown.Size = new Size(75, 23);
+            btn_scrollDown.TabIndex = 7;
+            btn_scrollDown.Text = "Down";
+            btn_scrollDown.UseVisualStyleBackColor = true;
+            btn_scrollDown.Click += btn_action_Click;
+            // 
+            // btn_scrollUp
+            // 
+            btn_scrollUp.Location = new Point(92, 141);
+            btn_scrollUp.Name = "btn_scrollUp";
+            btn_scrollUp.Size = new Size(75, 23);
+            btn_scrollUp.TabIndex = 6;
+            btn_scrollUp.Text = "Up";
+            btn_scrollUp.UseVisualStyleBackColor = true;
+            btn_scrollUp.Click += btn_action_Click;
+            // 
+            // btn_clickRight
+            // 
+            btn_clickRight.Location = new Point(205, 84);
+            btn_clickRight.Name = "btn_clickRight";
+            btn_clickRight.Size = new Size(75, 23);
+            btn_clickRight.TabIndex = 5;
+            btn_clickRight.Text = "Right";
+            btn_clickRight.UseVisualStyleBackColor = true;
+            btn_clickRight.Click += btn_action_Click;
+            // 
+            // btn_clickLeft
+            // 
+            btn_clickLeft.Location = new Point(92, 84);
+            btn_clickLeft.Name = "btn_clickLeft";
+            btn_clickLeft.Size = new Size(75, 23);
+            btn_clickLeft.TabIndex = 4;
+            btn_clickLeft.Text = "Left";
+            btn_clickLeft.UseVisualStyleBackColor = true;
+            btn_clickLeft.Click += btn_action_Click;
+            // 
+            // btn_moveGradual
+            // 
+            btn_moveGradual.Location = new Point(92, 24);
+            btn_moveGradual.Name = "btn_moveGradual";
+            btn_moveGradual.Size = new Size(75, 23);
+            btn_moveGradual.TabIndex = 3;
+            btn_moveGradual.Text = "Gradual";
+            btn_moveGradual.UseVisualStyleBackColor = true;
+            btn_moveGradual.Click += btn_action_Click;
+            // 
+            // lbl_scroll
+            // 
+            lbl_scroll.AutoSize = true;
+            lbl_scroll.Location = new Point(15, 145);
+            lbl_scroll.Name = "lbl_scroll";
+            lbl_scroll.Size = new Size(39, 15);
+            lbl_scroll.TabIndex = 2;
+            lbl_scroll.Text = "Scroll:";
+            // 
+            // lbl_click
+            // 
+            lbl_click.AutoSize = true;
+            lbl_click.Location = new Point(14, 88);
+            lbl_click.Name = "lbl_click";
+            lbl_click.Size = new Size(36, 15);
+            lbl_click.TabIndex = 1;
+            lbl_click.Text = "Click:";
+            // 
+            // lbl_move
+            // 
+            lbl_move.AutoSize = true;
+            lbl_move.Location = new Point(14, 28);
+            lbl_move.Name = "lbl_move";
+            lbl_move.Size = new Size(40, 15);
+            lbl_move.TabIndex = 0;
+            lbl_move.Text = "Move:";
             // 
             // Frm_MouseDummy
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(599, 450);
+            ClientSize = new Size(599, 310);
             Controls.Add(grpbx_action);
             Controls.Add(grpbx_pointSelector);
             Controls.Add(grpbx_points);
             Name = "Frm_MouseDummy";
             Text = "Mouse Dummy";
+            Load += Frm_MouseDummy_Load;
             grpbx_points.ResumeLayout(false);
             grpbx_pointSelector.ResumeLayout(false);
             grpbx_pointSelector.PerformLayout();
             grpbx_action.ResumeLayout(false);
-            tabControl1.ResumeLayout(false);
+            grpbx_action.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -196,13 +291,21 @@
         private GroupBox grpbx_action;
         private ListBox lstbx_points;
         private Label lbl_x;
-        private TextBox textBox2;
-        private TextBox textBox1;
-        private Button btn_trackMouseCoor;
+        private TextBox txtbx_x;
+        private TextBox txtbx_y;
+        private Button btn_trackMousePos;
         private Button btn_add;
         private Label lbl_y;
-        private TabControl tabControl1;
-        private TabPage tabPage1;
-        private TabPage tabPage2;
+        private ComboBox cmbbx_savedSecuences;
+        private Label lbl_click;
+        private Button btn_scrollDown;
+        private Button btn_scrollUp;
+        private Button btn_clickRight;
+        private Button btn_clickLeft;
+        private Button btn_moveGradual;
+        private Label lbl_scroll;
+        private Button btn_moveInstant;
+        private Label lbl_move;
+        private Button btn_playSecuence;
     }
 }
--- MouseDummy/Frm_MouseDummy.cs
+++ MouseDummy/Frm_MouseDummy.cs
@@ -6,5 +6,30 @@ namespace MouseDummy
         {
             InitializeComponent();
         }
+
+        private void Frm_MouseDummy_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_trackMousePos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbbx_savedSecuences_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void btn_action_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
